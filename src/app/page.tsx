0a400d8b9feab400d29442b31c9392f7006c4f79
Kim Jimin <jiminido1@gmail.com>
2025-05-21 14:53:32 +0900

    chore: error 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { AiOutlineLike, AiOutlineComment, AiOutlineRetweet, AiOutlineShareAlt } from "react-icons/ai";
 import Image from "next/image";
 import Header from "@/components/Header";
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { MdGeneratingTokens, MdOutlineMood } from "react-icons/md";
 
 type ThemeMode = 'default' | 'token';
